4cf52c6c98ac887171ef5f29a9443de67ebb901f
Diomar-s <diomarlugo405@gmail.com>
2024-08-23 03:16:12 -0400

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `ꨄ ¡𝙷𝚘𝚕𝚊 𝚜𝚘𝚢 𝚗𝚊𝚔𝚊𝚗𝚘-𝙱𝚘𝚝, 𝚎𝚗 𝚚𝚞𝚎 𝚙𝚞𝚎𝚍𝚘 𝚊𝚢𝚞𝚍𝚊𝚛𝚝𝚎 𝚑𝚘𝚢?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `ꨄ ¡𝑯𝒐𝒍𝒂 𝒔𝒐𝒚 𝑫𝒓𝒂𝒌𝒐-𝑩𝒐𝒕, 𝒆𝒏 𝒒𝒖𝒆 𝒑𝒖𝒆𝒅𝒐 𝒂𝒚𝒖𝒅𝒂𝒓 𝒉𝒐𝒚?\n\n✰ 𝒖𝒔𝒂 *!𝒎𝒆𝒏𝒖* 𝒑𝒂𝒓𝒂 𝒗𝒆𝒓 𝒕𝒐𝒅𝒂𝒔 𝒎𝒊𝒔 𝒇𝒖𝒏𝒄𝒊𝒐𝒏𝒆𝒔`, m, rcanal, )
 }
 /*if (/^que|q$/i.test(m.text)) {
 conn.reply(m.chat, `*so y arroz* 👌🤌`, m, rcanal, )
@@ -16,7 +16,7 @@ conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
 /*if (/^a$/i.test(m.text)) {
 conn.reply(m.chat, `*rroz y pollo* 😄👌`, m, rcanal, )
 }*/
-if (!chat.isBanned && m.text.match(/(te amo|teamo|te amo bot|te amo nakano)/gi)) {
+if (!chat.isBanned && m.text.match(/(te amo|teamo|te amo bot|te amo 𝑫𝒓𝒂𝒌𝒐)/gi)) {
   conn.sendMessage(m.chat, {
  stiker:{ url: global.stickeramor}
 }, { quoted: fakegif2});
