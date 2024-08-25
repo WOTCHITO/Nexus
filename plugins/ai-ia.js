f72ab94aad7cde6e2972aca474129ddb5f426bc4
Diomar-s <diomarlugo405@gmail.com>
2024-08-25 10:02:13 -0400

    Update ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es Megumin-Bot y parece haber sido creado por David Chian. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender y sobre todo las explociones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es 𝐃𝐫𝐚𝐤𝐨-𝐁𝐨𝐭 y parece haber sido creado por 𝐊𝐢𝐧𝐠 𝐁𝐨𝐭. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender y sobre todo las explociones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 if (isQuotedImage) {
 const q = m.quoted
 const img = await q.download?.()
