1209a9f5b08e2be529493e08074fa9e86305bed1
마이콜렉시즈 <maicolxyz666@gmail.com>
2024-08-27 10:17:58 -0400

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,9 +1,8 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text
-conn.reply(m.chat, lenguajeGB.smsSetW(), fkontak, m)
-//conn.sendButton(m.chat, wm, lenguajeGB['smsSetW'](), null, [[lenguajeGB.smsConMenu(), `/menu`]], fkontak, m)
-} else throw `${lenguajeGB['smsSetW2']()}`
+let bienvenida = `BIENVENIDO/A`
+conn.reply(m.chat, bienvenida, fkontak, m)}
 }
 handler.command = ['setwelcome', 'bienvenida'] 
 handler.botAdmin = true
