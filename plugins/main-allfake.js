436809148090ebc3ca16e5b025e76fbd516cd5a3
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-08-27 23:43:40 -0500

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -97,7 +97,7 @@ global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@
 
 // global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363323286489957@newsletter', newsletterName: "⏤͟͞ू𝐃𝐫𝐚𝐤𝐨๛✎𝐁𝐨𝐭❄︎✍︎", serverMessageId: -1 }
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363218713824827@newsletter', newsletterName: "⏤͟͞ू𝐃𝐫𝐚𝐤𝐨๛✎𝐁𝐨𝐭❄︎✍︎", serverMessageId: -1 }
 }}, { quoted: m }
 
 global.icono = [ 
