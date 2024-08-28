97257683fb78386770d2142c1e9e804d58dbc64d
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-08-27 23:22:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -107,7 +107,7 @@ global.comunidad1 = 'https://chat.whatsapp.com/Edih0ePD45VKCwmAbkretr' //Comunid
 global.channel = 'https://whatsapp.com/channel/0029VaFMguNDTkJzaPGXAG3D' //Canal Oficial
 global.yt = 'https://youtube.com/@nexuss-bot?si=oiEtEYkYKGkEBtai' //Canal De Youtube
 global.md = 'https://github.com/Diomar-s/Drako-king-12-.git' //Github Oficial
-global.correo = 'lugodiomar6@gmail.com'
+global.correo = 'bastojorgue@gmail.com'
 global.cn ='https://whatsapp.com/channel/0029VaFMguNDTkJzaPGXAG3D';
 
 var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Linda Mañana'; break; case 1: hour = 'Linda Mañana'; break; case 2: hour = 'Linda Mañana'; break; case 3: hour = 'Linda Mañana'; break; case 4: hour = 'linda mañana'; break; case 5: hour = 'Linda Mañana'; break; case 6: hour = 'Linda Mañana'; break; case 7: hour = 'Linda Mañana'; break; case 8: hour = 'Linda Mañana'; break; case 9: hour = 'Linda Mañana'; break; case 10: hour = 'Lindo Dia'; break; case 11: hour = 'Lindo Dia'; break; case 12: hour = 'Lindo Dia'; break; case 13: hour = 'Lindo Dia'; break; case 14: hour = 'Linda Tarde'; break; case 15: hour = 'Linda Tarde'; break; case 16: hour = 'Linda Tarde'; break; case 17: hour = 'Linda Tarde'; break; case 18: hour = 'Linda Noche'; break; case 19: hour = 'Linda Noche'; break; case 20: hour = 'Linda Noche'; break; case 21: hour = 'Linda Noche'; break; case 22: hour = 'Linda Noche'; break; case 23: hour = 'Linda Noche'; break;}
