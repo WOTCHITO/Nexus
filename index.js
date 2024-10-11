15bac42369463ffffcb23d11005e01e84fa44b15
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-10-10 19:39:18 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Megumin ✰');
+console.log('\n✰ Iniciando Nexus ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Megumin-Bot', {
+say('Nexus-Bot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • David-Chian`, {
+say(`Developed By • Wotchito`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
@@ -77,4 +77,4 @@ console.warn('🚩 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
-start('start.js');
+start('sunlight.js');
